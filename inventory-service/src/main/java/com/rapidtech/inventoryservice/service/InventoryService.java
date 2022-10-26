21857b41c156d4c366faf8a71e41a33b014d773c
ekurniawan <erick.kurniawan@gmail.com>
2022-10-26 14:29:46 +0700

    menggunakan library resiliance4j untuk circuit braker pattern

--- inventory-service/src/main/java/com/rapidtech/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/rapidtech/inventoryservice/service/InventoryService.java
@@ -5,16 +5,23 @@ import com.rapidtech.inventoryservice.dto.InventoryResponse;
 import com.rapidtech.inventoryservice.model.Inventory;
 import com.rapidtech.inventoryservice.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class InventoryService {
     private final InventoryRepository inventoryRepository;
 
+    @SneakyThrows
     public List<InventoryResponse> isInStock(List<String> skuCode){
+        log.info("Mulai menunggu");
+        Thread.sleep(10000);
+        log.info("Selesai menunggu");
         return inventoryRepository.findBySkuCodeIn(skuCode).stream()
                 .map(inventory ->
                     InventoryResponse.builder()
--- order-service/src/main/java/com/rapidtech/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/rapidtech/orderservice/service/OrderService.java
@@ -21,7 +21,7 @@ public class OrderService {
     private final OrderRepository orderRepository;
     private final WebClient.Builder webClientBuilder;
 
-    public void placeOrder(OrderRequest orderRequest){
+    public String placeOrder(OrderRequest orderRequest){
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
         List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDtoList()
@@ -44,6 +44,7 @@ public class OrderService {
 
         if(allProductsInStock){
             orderRepository.save(order);
+            return "Order berhasil dilakukan...";
         }else {
             throw new IllegalArgumentException("Stok Product tidak mencukupi....");
         }
