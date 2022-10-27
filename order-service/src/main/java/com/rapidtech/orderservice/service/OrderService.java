c3e482f35cb33c05872fb4e75a81792efd9ae628
ekurniawan <erick.kurniawan@gmail.com>
2022-10-27 10:30:55 +0700

    menambahkan docker compose kafka

--- order-service/src/main/java/com/rapidtech/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/rapidtech/orderservice/service/OrderService.java
@@ -7,8 +7,12 @@ import com.rapidtech.orderservice.model.Order;
 import com.rapidtech.orderservice.model.OrderLineItems;
 import com.rapidtech.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.sleuth.Span;
+import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import zipkin2.internal.Trace;
 
 import java.lang.reflect.Array;
 import java.util.Arrays;
@@ -17,9 +21,12 @@ import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class OrderService {
     private final OrderRepository orderRepository;
     private final WebClient.Builder webClientBuilder;
+
+    private final Tracer tracer;
 
     public String placeOrder(OrderRequest orderRequest){
         Order order = new Order();
@@ -31,22 +38,27 @@ public class OrderService {
 
         List<String> skuCodes = order.getOrderLineItemsList().stream()
                 .map(OrderLineItems::getSkuCode).toList();
+        log.info("Memanggil inventory service");
 
-        //cek di bagian inventory
-        InventoryResponse[] inventoryResponsesArr = webClientBuilder.build().get().uri("http://inventory-service/api/inventory",
-                uriBuilder -> uriBuilder.queryParam("skuCode",skuCodes).build())
-                .retrieve()
-                .bodyToMono(InventoryResponse[].class)
-                .block();
+        Span inventoryServiceLookup = tracer.nextSpan().name("InventoryServiceLookup");
+        try (Tracer.SpanInScope isLookup = tracer.withSpan(inventoryServiceLookup.start())){
+            inventoryServiceLookup.tag("call","inventory-service");
+            //cek di bagian inventory
+            InventoryResponse[] inventoryResponsesArr = webClientBuilder.build().get().uri("http://inventory-service/api/inventory",
+                            uriBuilder -> uriBuilder.queryParam("skuCode",skuCodes).build())
+                    .retrieve()
+                    .bodyToMono(InventoryResponse[].class)
+                    .block();
 
-        boolean allProductsInStock =
-                Arrays.stream(inventoryResponsesArr).allMatch(InventoryResponse::isInStock);
+            boolean allProductsInStock =
+                    Arrays.stream(inventoryResponsesArr).allMatch(InventoryResponse::isInStock);
 
-        if(allProductsInStock){
-            orderRepository.save(order);
-            return "Order berhasil dilakukan...";
-        }else {
-            throw new IllegalArgumentException("Stok Product tidak mencukupi....");
+            if(allProductsInStock){
+                orderRepository.save(order);
+                return "Order berhasil dilakukan...";
+            }else {
+                throw new IllegalArgumentException("Stok Product tidak mencukupi....");
+            }
         }
     }
 
